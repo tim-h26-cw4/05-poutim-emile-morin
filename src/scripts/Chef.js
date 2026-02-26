966ba249d40b3b5b4f3bd1162a28fa6637f3f1e7
Jean-Michele <morin.emilemorin@gmail.com>
2026-02-26 08:55:35 -0500

    Chef.js, partiellement finis, commencement de poutine.js

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -1,4 +1,21 @@
 export default class Chef {
-  constructor(element) {}
-  init() {}
+  constructor(element) {
+    this.element = element;
+    this.menu = [];
+    this.container = document.querySelectorAll('.chef__order');
+    console.log('yep');
+    this.init();
+  }
+  init() {
+    let poutine = this.element;
+    this.menu.push(poutine);
+    const boutons = document.querySelectorAll('.button-secondary');
+    for (let i = 0; i < boutons.length; i++) {
+      const bouton = boutons[i];
+      bouton.addEventListener('click', this.sendOrder);
+    }
+  }
+  sendOrder() {
+    console.log('yep');
+  }
 }
